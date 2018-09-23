411582dd931adf2e425ab2b4956e707d80c87a65
HOJONG_LAPTOP <jeontha@gmail.com>
2018-09-23 17:31:50 +0900

    xxhdpi

--- app/src/main/java/com/hojong/meokgol/activity/LogoActivity.java
+++ app/src/main/java/com/hojong/meokgol/activity/LogoActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.hojong.meokgol.R;
 
